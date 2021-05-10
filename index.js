3aa4a0506e71166d082cf3df8405fbafd31755b8
Arnav Borborah <arnavborborah11@gmail.com>
2021-05-10 11:14:27 -0400

    3.8: Log data sent in POST requests

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("post-body", (request, response) =>
+  request.method === "POST" ? JSON.stringify(request.body) : ""
+);
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-body"
+  )
+);
 
 let persons = [
   {

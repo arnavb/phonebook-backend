e7bb333fc37056142d8f0b631806614352ff161f
Arnav Borborah <arnavborborah11@gmail.com>
2021-05-11 08:21:00 -0400

    Add functionality to fetch individual ids

--- index.js
+++ index.js
@@ -82,15 +82,18 @@ app.post("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.json(result);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformed id" });
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
